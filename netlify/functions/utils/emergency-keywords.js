9925150e2b2ffe067c67579d30c74d91f092c4c3
dpd653 <dpd653@gmail.com>
2026-08-09 18:01:42 -0400

    Expand emergency keyword list

--- netlify/functions/utils/emergency-keywords.js
+++ netlify/functions/utils/emergency-keywords.js
@@ -4,11 +4,16 @@
 // through silently when a contractor forgets to check the box.
 
 const EMERGENCY_KEYWORDS = [
-  "no heat", "no power", "no electricity", "no water", "no hot water",
+  "no heat", "no power", "no electric", "no electricity", "power out", "power outage",
+  "no water", "no hot water",
   "flooding", "flooded", "flood", "burst pipe", "pipe burst", "leaking",
-  "gas leak", "smell gas", "sparking", "sparks", "smoke", "fire",
-  "emergency", "urgent", "asap", "right away", "can't wait",
-  "sewage", "backed up", "overflow", "overflowing",
+  "water leak", "leak", "water line", "line break", "broken pipe", "pipe broke",
+  "furnace out", "furnace went out", "furnace not working", "furnace broken",
+  "gas leak", "smell gas", "smells like gas", "sparking", "sparks", "smoke", "fire",
+  "emergency", "urgent", "asap", "right away", "can't wait", "immediately",
+  "sewage", "sewage backup", "sewage back up", "backed up", "overflow", "overflowing",
+  "storm damage", "tree damage", "tree fell", "tree down", "fallen tree",
+  "roof damage", "roof leak", "water damage", "ceiling collapse",
 ];
 
 function messageContainsEmergencyKeyword(message) {
